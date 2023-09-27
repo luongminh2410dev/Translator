d0644f28319e1a7cd72c6c9e952de5543b4b0ae7
Luong Minh <virutphalangxa@gmail.com>
2023-09-27 23:19:35 +0700

    update

--- ios/translate_appTests/translate_appTests.m
+++ ios/translate_appTests/translate_appTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface translatorTests : XCTestCase
+@interface translate_appTests : XCTestCase
 
 @end
 
-@implementation translatorTests
+@implementation translate_appTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
--- src/screens/ImageTranslator/index.tsx
+++ src/screens/ImageTranslator/index.tsx
@@ -1,8 +1,24 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {Dimensions, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import styles from './styles';
+import ImagePicker from 'react-native-image-crop-picker';
 
+const {width, height} = Dimensions.get('screen');
 const ImageTranslator = () => {
+  const [translateItems, setTranslateItems] = useState([]);
+
+  useEffect(() => {
+    ImagePicker.openCamera({
+      width: width,
+      height: height,
+    }).then(image => {
+      console.log(image);
+    });
+    return () => {
+      ImagePicker.clean();
+    };
+  }, []);
+
   return (
     <View>
       <Text>ImageTranslator</Text>
